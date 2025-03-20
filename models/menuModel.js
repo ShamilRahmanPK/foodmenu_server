55381b04df0f08c989e820b67282a7f4de6fe429
Shamil Rahman PK <shamilrahman7994@gmail.com>
2025-03-20 23:41:18 +0530

    first commit

--- models/menuModel.js
+++ models/menuModel.js
@@ -6,9 +6,9 @@ mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log('✅ Connected to MongoDB');
+    console.log('Connected to MongoDB');
 }).catch(err => {
-    console.error('❌ MongoDB Connection Error:', err);
+    console.error('MongoDB Connection Error:', err);
 });
 
 const menuItemSchema = new mongoose.Schema({
